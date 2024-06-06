1c8921a6e5a630d6d311075ffc09a5b03f57e1b9
Jeremy Jee <jeremy_jee@tech.gov.sg>
2024-06-07 01:08:57 +0800

    Added types to Products

--- src/app/_components/products/organisation-products-list.tsx
+++ src/app/_components/products/organisation-products-list.tsx
@@ -1,20 +1,37 @@
 "use client";
 
-import { Stack, Group, Title, Button } from "@mantine/core";
+import { Stack, Group, Title, Button, SegmentedControl } from "@mantine/core";
 import { IconPlus } from "@tabler/icons-react";
 import Link from "next/link";
 import { navLinks } from "../shell";
 import { ProductCardsGrid } from "./product-cards-grid";
 import classes from "~/styles/title.module.css";
+import segmentedClasses from "~/styles/segmented.module.css";
 import { type OrganisationProducts } from "~/server/api/routers/organisation";
+import { useState } from "react";
 
 export function OrganisationProductsList({
   organisations,
 }: {
   organisations: OrganisationProducts[];
 }) {
+  const [type, setType] = useState("PRODUCT");
+
   return (
     <Stack>
+      <SegmentedControl
+        radius="xl"
+        size="md"
+        data={[
+          { label: "Govt Product", value: "PRODUCT" },
+          { label: "Agency Project", value: "AGENCY" },
+          { label: "Development Tool", value: "DEVTOOL" },
+          { label: "Innersource Project", value: "INNERSOURCE" },
+          { label: "Prototype", value: "PROTOTYPE" },
+        ]}
+        onChange={(value) => setType(value)}
+        classNames={segmentedClasses}
+      />
       <Group justify="space-between" mb="md">
         <Title order={1} c="white" className={classes.titleUnderline}>
           GovTech Products
@@ -29,10 +46,24 @@ export function OrganisationProductsList({
           Create
         </Button>
       </Group>
-      {organisations.map((organisation) =>
-        organisation.products.length > 0 ||
-        organisation.children.map((child) => child.products).flat().length >
-          0 ? (
+      {organisations.map((organisation) => {
+        const orgProducts = organisation.products.filter(
+          (product) => product.type === type,
+        );
+
+        const hasProducts =
+          orgProducts.length > 0 ||
+          organisation.children
+            .map((child) =>
+              child.products.filter((product) => product.type === type),
+            )
+            .flat().length > 0;
+
+        if (!hasProducts) {
+          return null;
+        }
+
+        return (
           <Stack key={organisation.id}>
             <Title
               order={2}
@@ -47,22 +78,37 @@ export function OrganisationProductsList({
                 ? "Non-GovTech Products"
                 : organisation.name}
             </Title>
-            <ProductCardsGrid products={organisation.products} />
+            <ProductCardsGrid
+              products={organisation.products.filter(
+                (product) => product.type === type,
+              )}
+            />
             <Stack ml="xl">
-              {organisation.children.map((childOrg) =>
-                childOrg.products.length > 0 ? (
+              {organisation.children.map((childOrg) => {
+                const childOrgProducts = childOrg.products.filter(
+                  (product) => product.type === type,
+                );
+
+                if (childOrgProducts.length === 0) {
+                  return null;
+                }
+
+                return (
                   <>
-                    <Title order={3} c="white">
+                    <Title key={childOrg.id + "-name"} order={3} c="white">
                       {childOrg.name}
                     </Title>
-                    <ProductCardsGrid products={childOrg.products} />
+                    <ProductCardsGrid
+                      key={childOrg.id + "-grid"}
+                      products={childOrgProducts}
+                    />
                   </>
-                ) : null,
-              )}
+                );
+              })}
             </Stack>
           </Stack>
-        ) : null,
-      )}
+        );
+      })}
     </Stack>
   );
 }
